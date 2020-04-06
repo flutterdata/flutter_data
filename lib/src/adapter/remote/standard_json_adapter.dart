35ccf66d7dddfb893afd5a4c4935fea830f3f921
Frank Treacy <me@frank06.net>
2020-04-06 10:32:40 -0300

    revamp standard json adapter

--- lib/src/adapter/remote/standard_json_adapter.dart
+++ lib/src/adapter/remote/standard_json_adapter.dart
@@ -1,134 +1,82 @@
-// import 'package:flutter_data/flutter_data.dart';
-// import 'package:recase/recase.dart';
+import 'package:flutter_data/flutter_data.dart';
+import 'package:recase/recase.dart';
 
-// mixin StandardJSONAdapter<T extends DataSupport<T>> on RemoteAdapter<T> {
-//   @override
-//   get headers => {'Content-Type': 'application/json'};
+mixin StandardJSONAdapter<T extends DataSupport<T>> on RemoteAdapter<T> {
+  @override
+  get headers => super.headers..addAll({'Content-Type': 'application/json'});
 
-//   String get identifier => 'id';
-//   String get identifierSuffix => '_id';
+  String get identifier => 'id';
+  String get identifierSuffix => '_id';
 
-//   String serializeKey(String key) => ReCase(key).snakeCase;
-//   String deserializeKey(String key) => ReCase(key).camelCase;
+  // String serializeKey(String key) => ReCase(key).snakeCase;
+  // String deserializeKey(String key) => ReCase(key).camelCase;
 
-//   // Transforms JSON:API into standard JSON
-//   @override
-//   dynamic serialize(Map<String, dynamic> json) {
-//     Map<String, dynamic> map;
-//     var data = json['data'];
-//     if (data is Map) {
-//       map = {
-//         ...{identifier: data['id']},
-//         ...data['attributes'].map((key, attr) {
-//           return MapEntry(serializeKey(key.toString()), attr);
-//         }),
-//         ...?data['relationships']?.map((k, v) {
-//           var key = serializeKey(k.toString());
-//           var relData = v['data'];
-//           if (relData is List) {
-//             return MapEntry(key, relData.map((e) => e[identifier]));
-//           } else {
-//             return MapEntry(key, relData[identifier]);
-//           }
-//         })
-//       };
-//     }
-//     return map;
-//   }
+  // var belongsToKey =
+  //         entry.key.replaceFirst(RegExp('$identifierSuffix\\b'), "");
 
-//   // Transforms standard JSON into JSON:API
-//   // TODO could it be constructed from json-api-dart classes instead?
-//   @override
-//   Map<String, dynamic> deserialize(dynamic json,
-//       [Map<String, dynamic> relationshipMetadata]) {
-//     dynamic primaryData;
-//     final includes = [];
+  // Transforms a class into standard JSON
+  @override
+  Map<String, dynamic> serialize(T model) {
+    final map = super.serialize(model);
 
-//     if (json is List) {
-//       primaryData = json
-//           .map((e) => _buildPrimaryData(
-//                 e as Map<String, dynamic>,
-//                 relationshipMetadata,
-//                 type,
-//               ))
-//           .toList();
-//       includes.addAll((primaryData as List).map((e) => e));
-//     } else if (json is Map<String, dynamic>) {
-//       primaryData = _buildPrimaryData(json, relationshipMetadata, type);
-//       includes.addAll(primaryData.remove('included') as Iterable);
-//     }
+    final relationships = <String, dynamic>{};
 
-//     return super.deserialize({'data': primaryData, 'included': includes});
-//   }
+    for (var relEntry in relationshipMetadata['HasMany'].entries) {
+      final name = relEntry.key.toString();
+      final keys = List<String>.from(map[name] as Iterable);
+      final type = relEntry.value;
+      relationships[name] = DataId.byKeys(keys, manager, type: type.toString())
+          .map((dataId) => dataId.id);
+      map.remove(name);
+    }
 
-//   Map<String, dynamic> _buildPrimaryData(Map<String, dynamic> json,
-//       Map<String, dynamic> relationshipMetadata, String type) {
-//     dynamic id;
-//     final attributes = <String, Object>{};
-//     final relationships = <String, Object>{};
-//     final included = [];
+    for (var relEntry in relationshipMetadata['BelongsTo'].entries) {
+      final name = '${relEntry.key}$identifierSuffix';
+      final key = map[name].toString();
+      final type = relEntry.value;
+      relationships[name] =
+          DataId.byKey(key, manager, type: type.toString()).id;
+      map.remove(name);
+    }
 
-//     final hasManys =
-//         Map<String, String>.from(relationshipMetadata['HasMany'] as Map);
-//     final belongsTos =
-//         Map<String, String>.from(relationshipMetadata['BelongsTo'] as Map);
+    final json = map;
+    json.addAll(relationships);
 
-//     for (var entry in json.entries) {
-//       var belongsToKey =
-//           entry.key.replaceFirst(RegExp('$identifierSuffix\\b'), "");
-//       var newKey = deserializeKey(entry.key);
+    return json;
+  }
 
-//       if (hasManys.containsKey(entry.key)) {
-//         relationships[newKey] = {
-//           'data': (entry.value as Iterable).map(
-//             (e) {
-//               var _isIncluded = e is Map;
-//               if (_isIncluded) {
-//                 included.add(_buildPrimaryData(
-//                   e as Map<String, dynamic>,
-//                   relationshipMetadata,
-//                   hasManys[entry.key],
-//                 ));
-//               }
-//               return _buildLinkage(
-//                 (_isIncluded ? e[identifier] : e).toString(),
-//                 hasManys[entry.key],
-//               );
-//             },
-//           ).toList()
-//         };
-//       } else if (belongsTos.containsKey(belongsToKey)) {
-//         var _isIncluded = entry.value is Map;
-//         if (_isIncluded) {
-//           included.add(_buildPrimaryData(
-//             entry.value as Map<String, dynamic>,
-//             relationshipMetadata,
-//             belongsTos[belongsToKey],
-//           ));
-//         }
-//         relationships[deserializeKey(belongsToKey)] = {
-//           'data': _buildLinkage(
-//             (_isIncluded ? entry.value[identifier] : entry.value).toString(),
-//             belongsTos[belongsToKey],
-//           )
-//         };
-//       } else if (entry.key == identifier) {
-//         id = entry.value.toString();
-//       } else {
-//         // attributes
-//         attributes[newKey] = entry.value;
-//       }
-//     }
-//     return {
-//       'id': id,
-//       'type': type,
-//       'attributes': attributes,
-//       'relationships': relationships,
-//       'included': included
-//     };
-//   }
+  // Transforms standard JSON into a class
+  @override
+  T deserialize(object, {key}) {
+    final obj = object as Map<String, dynamic>;
 
-//   _buildLinkage(id, type) {
-//     return {'id': id, 'type': type};
-//   }
-// }
+    for (var relEntry in relationshipMetadata['HasMany'].entries) {
+      final k = relEntry.key.toString();
+      obj[k] = obj[k].map((i) {
+        final type = relEntry.value.toString();
+        if (i is Map) {
+          final repo = relationshipMetadata['repository#$type'] as Repository;
+          final model = repo.deserialize(i);
+          i = model.id;
+        }
+        final dataId = manager.dataId(i.toString(), type: type);
+        return dataId.key;
+      }).toList();
+    }
+
+    for (var relEntry in relationshipMetadata['BelongsTo'].entries) {
+      final k = relEntry.key.toString();
+      final type = relEntry.value.toString();
+      if (obj[k] is Map) {
+        final repo = relationshipMetadata['repository#$type'] as Repository;
+        final model = repo.deserialize(obj[k]);
+        obj[k] = model.id;
+      }
+
+      final dataId = manager.dataId(obj[k].toString(), type: type);
+      obj[k] = dataId.key;
+    }
+
+    return super.deserialize(obj);
+  }
+}
