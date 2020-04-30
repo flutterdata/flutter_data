af7021885ec34493c975d843b64147b8862df1ed
Frank Treacy <me@frank06.net>
2020-04-29 22:20:06 -0300

    add params for potential reinit API

--- lib/src/model/data/data_support.dart
+++ lib/src/model/data/data_support.dart
@@ -97,7 +97,9 @@ abstract class DataSupport<T extends DataSupport<T>> with DataSupportMixin<T> {
 extension DataSupportExtension<T extends DataSupport<T>> on DataSupport<T> {
   @Deprecated('Do not call init() when this model extends DataSupport')
   // ignore: missing_return
-  T init() {}
+  T init({String key, bool save = true}) {
+    return _repository._init(_this, key: key, save: save);
+  }
 }
 
 mixin IdDataSupportMixin<ID, T extends DataSupportMixin<T>>
