9112d073abd4fcb065e5aeca16830d3f76f32c0a
Frank Treacy <me@frank06.net>
2020-05-18 15:42:25 -0300

    add throttle to notifier extensions

--- lib/src/util/notifier_extension.dart
+++ lib/src/util/notifier_extension.dart
@@ -9,6 +9,8 @@ class _FunctionalStateNotifier<T> extends DataStateNotifier<T> {
   final DataStateNotifier<T> _source;
   _FunctionalStateNotifier(this._source) : super(null);
   RemoveListener _disposeFn;
+  Timer _timer;
+  bool _dirty = false;
 
   DataStateNotifier<T> where(bool Function(DataState<T>) test) {
     _disposeFn = _source.addListener((_state) {
@@ -30,8 +32,39 @@ class _FunctionalStateNotifier<T> extends DataStateNotifier<T> {
     return this;
   }
 
+  DataState<T> _unthrottledState;
+
+  DataStateNotifier<T> throttle(int delay) {
+    _disposeFn = _source.addListener((state) {
+      if (_timer == null) {
+        _dirty = false;
+        super.state = state;
+        _timer = _makeTimer(delay);
+      } else {
+        _dirty = true;
+      }
+      _unthrottledState = state;
+    });
+    return this;
+  }
+
+  Timer _makeTimer(int delay) {
+    return Timer(Duration(seconds: delay), () {
+      if (mounted) {
+        if (_dirty) {
+          _dirty = false;
+          _timer = _makeTimer(delay);
+          super.state = _unthrottledState;
+        } else {
+          _timer = null;
+        }
+      }
+    });
+  }
+
   @override
   void dispose() {
+    _timer?.cancel();
     _disposeFn?.call();
     super.dispose();
   }
@@ -49,6 +82,11 @@ extension WhereDataStateNotifierX<T> on DataStateNotifier<T> {
   void forEach(void Function(DataState<T>) action) {
     return _FunctionalStateNotifier<T>(this).forEach(action);
   }
+
+  // updates state maximum once per [delay]
+  DataStateNotifier<T> throttle(int delay) {
+    return _FunctionalStateNotifier<T>(this).throttle(delay);
+  }
 }
 
 class TestStateNotifier extends DataStateNotifier<int> {
@@ -61,7 +99,8 @@ class TestStateNotifier extends DataStateNotifier<int> {
 void main() {
   final notifier = TestStateNotifier()
       .map((state) => state.copyWith(model: state.model * 3))
-      .where((state) => state.model % 2 == 0);
+      .where((state) => state.model % 2 == 0)
+      .throttle(1);
   notifier.addListener((state) => print(state.model));
   // notifier.dispose();
 }
