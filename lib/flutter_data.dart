70951fe2af0a53a98ea48f1c12522126712b37a3
Frank Treacy <me@frank06.net>
2021-03-09 17:35:01 -0300

    fix libraries

--- lib/flutter_data.dart
+++ lib/flutter_data.dart
@@ -1,11 +1,11 @@
 library flutter_data;
 
+// import external packages
 import 'dart:async';
 import 'dart:collection';
 import 'dart:convert';
-import 'package:data_state/data_state.dart';
 import 'package:equatable/equatable.dart';
-import 'package:riverpod/all.dart';
+import 'package:riverpod/riverpod.dart';
 import 'package:state_notifier/state_notifier.dart';
 import 'package:meta/meta.dart';
 import 'package:http/http.dart' as http;
@@ -15,14 +15,20 @@ import 'package:uuid/uuid.dart';
 import 'package:path/path.dart' as path_helper;
 import 'package:collection/collection.dart';
 
+// import internal packages
 import 'src/graph/notifier_extension.dart';
 import 'src/repository/hive_local_storage.dart';
+import 'src/utils/data_state.dart';
 
-export 'package:data_state/data_state.dart';
-export 'package:riverpod/all.dart' hide Family, Listener;
+// export external packages
+export 'package:riverpod/riverpod.dart' hide Family, Listener;
+
+// export internal packages
 export 'src/graph/notifier_extension.dart';
 export 'src/repository/hive_local_storage.dart';
+export 'src/utils/data_state.dart';
 
+// include parts
 part 'src/model/data_model.dart';
 part 'src/model/relationship/relationship.dart';
 part 'src/model/relationship/has_many.dart';
