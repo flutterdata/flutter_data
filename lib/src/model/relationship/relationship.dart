a494e11d770e11b1e918190d1e3e9ba209f22233
Frank Treacy <me@frank06.net>
2021-03-15 22:51:34 -0300

    add collection-like methods to relationship

--- lib/src/model/relationship/relationship.dart
+++ lib/src/model/relationship/relationship.dart
@@ -75,7 +75,7 @@ abstract class Relationship<E extends DataModel<E>, N>
   @override
   bool get isInitialized => _ownerKey != null && _adapters != null;
 
-  // implement the `Set`
+  // implement collection-like methods
 
   /// Add a [value] to this [Relationship]
   ///
@@ -107,10 +107,6 @@ abstract class Relationship<E extends DataModel<E>, N>
 
   Iterator<E> get iterator => _iterable.iterator;
 
-  E lookup(Object element) {
-    return toSet().lookup(element);
-  }
-
   /// Removes a [value] from this [Relationship]
   bool remove(Object value, {bool notify = true}) {
     assert(value is E);
@@ -129,17 +125,21 @@ abstract class Relationship<E extends DataModel<E>, N>
     return _uninitializedModels.remove(model);
   }
 
+  E get first => _iterable.safeFirst;
+
   int get length => _iterable.length;
 
   bool get isEmpty => _iterable.isEmpty;
 
   bool get isNotEmpty => _iterable.isNotEmpty;
 
-  E get first => _iterable.safeFirst;
+  Iterable<E> where(bool Function(E) test) => _iterable.where(test);
 
-  Set<E> toSet() {
-    return _iterable.toSet();
-  }
+  Iterable<T> map<T>(T Function(E) f) => _iterable.map(f);
+
+  Set<E> toSet() => _iterable.toSet();
+
+  List<E> toList() => _iterable.toList();
 
   // support methods
 
